Corrected energy calculation (previously missing a 0.5 factor)

src/exhaustive_3_states_search.py
@@ -155,7 +155,7 @@ class ChargeConfig:
                 if self.v_i[i] == -float('inf'):
                     self._calc_v_i(i)
             self.v_i_ready = True
-        return np.inner(self.db_states, self.v_i)
+        return 0.5 * np.inner(self.db_states, self.v_i)
 
     def physically_valid(self):
         '''